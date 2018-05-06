072d698ac27874c5154ca41b961299b1064f05d6
rjw0205 <rjw0205@kaist.ac.kr>
2018-05-06 13:28:57 +0900

    user_list.txt involves 2290300 users

--- get_user.py
+++ get_user.py
@@ -13,7 +13,7 @@ def get_user_from_url(url):
             user_list.append(summoner_name)
     return user_list
 
-def get_user_list(start=1, end=22900, step=200):
+def get_user_list(start=1, end=22903, step=1):
     base_url = "http://www.op.gg/ranking/ladder/page={}"
     user_list = ['GabrielCRO', 'DWG ShowMaker', 'yizhiyu', 'MVP ADD', 'SKT T1 Bang']
     for page_num in range(start, end, step):
